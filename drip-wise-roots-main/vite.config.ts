b9a4b39c3a098c570b5be4b30b20a89c3ed8681f
PinnapureddyNithinreddy <Saikumar.Kolli@8thelement.ai>
2026-04-18 18:47:24 +0530

    latest changes

--- drip-wise-roots-main/vite.config.ts
+++ drip-wise-roots-main/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "./",   // 🔥 THIS IS THE FIX
+  base: "./",
   plugins: [react(), tsconfigPaths()],
 });
