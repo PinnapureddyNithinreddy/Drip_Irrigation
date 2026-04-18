8d2e36f8b062513b35639631300bde6c8ce72170
PinnapureddyNithinreddy <Saikumar.Kolli@8thelement.ai>
2026-04-18 18:17:06 +0530

    fix vercel file

--- drip-wise-roots-main/src/main.tsx
+++ drip-wise-roots-main/src/main.tsx
@@ -1,9 +1,10 @@
 // src/main.tsx
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
-import "./styles.css";
+
 
 const router = getRouter();
 
--- drip-wise-roots-main/vite.config.ts
+++ drip-wise-roots-main/vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "./",   // 🔥 THIS IS THE FIX
   plugins: [react(), tsconfigPaths()],
 });
